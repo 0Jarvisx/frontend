c632a33ba6c8b067b34809a58fb91b2ad1f20d83
0Jarvisx <104813692+0Jarvisx@users.noreply.github.com>
2024-09-03 22:52:22 -0600

    final 1

--- src/components/FileList.vue
+++ src/components/FileList.vue
@@ -66,7 +66,7 @@ export default {
   methods: {
     async refreshFiles() {
       try {
-        const response = await axios.get(`http://localhost:3000/files/${this.category}`);
+        const response = await axios.get(`http://localhost:3000/files/${this.category}`, {withCredentials: true});
         this.files = response.data;
         console.log('----------this.files', this.files);
       } catch (error) {
