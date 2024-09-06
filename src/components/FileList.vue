c38e248c496ff60fce2293a158401f69f1fb9429
0Jarvisx <104813692+0Jarvisx@users.noreply.github.com>
2024-09-05 23:48:21 -0600

    prueba de IAM

--- src/components/FileList.vue
+++ src/components/FileList.vue
@@ -66,7 +66,7 @@ export default {
   methods: {
     async refreshFiles() {
       try {
-        const response = await axios.get(`http://44.211.197.243:3000/files/${this.category}`, {withCredentials: false});
+        const response = await axios.get(`http://3.87.201.173:3000/files/${this.category}`, {withCredentials: false});
         this.files = response.data;
         console.log('----------this.files', this.files);
       } catch (error) {
--- src/components/UploadForm.vue
+++ src/components/UploadForm.vue
@@ -49,7 +49,7 @@ export default {
       this.isUploading = true; 
 
       try {
-        const response = await axios.post('http://44.211.197.243:3000/upload?category=' + this.category, formData, {withCredentials: false});
+        const response = await axios.post('http://3.87.201.173:3000/upload?category=' + this.category, formData, {withCredentials: false});
         console.log('File uploaded:', response.data);
         if (response.data.message) this.$emit('file-uploaded', this.category);
         
