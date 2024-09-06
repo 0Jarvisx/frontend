48d27ea84073586d2368a96b969e879d2a963db6
0Jarvisx <104813692+0Jarvisx@users.noreply.github.com>
2024-09-05 21:17:12 -0600

    Prueba con localhost

--- src/components/UploadForm.vue
+++ src/components/UploadForm.vue
@@ -49,7 +49,7 @@ export default {
       this.isUploading = true; 
 
       try {
-        const response = await axios.post('http://18.234.141.110:3000/upload?category=' + this.category, formData, {withCredentials: false});
+        const response = await axios.post('http://localhost:3000/upload?category=' + this.category, formData, {withCredentials: false});
         console.log('File uploaded:', response.data);
         if (response.data.message) this.$emit('file-uploaded', this.category);
         
