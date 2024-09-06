3b7fb5221e55e60983026690a523bfeefd85ed61
0Jarvisx <104813692+0Jarvisx@users.noreply.github.com>
2024-09-06 11:29:54 -0600

    Cambios para instancia 4

--- src/components/FileList.vue
+++ src/components/FileList.vue
@@ -66,7 +66,7 @@ export default {
   methods: {
     async refreshFiles() {
       try {
-        const response = await axios.get(`http://34.226.153.115:3000/files/${this.category}`, {withCredentials: false});
+        const response = await axios.get(`http://3.86.187.164:3000/files/${this.category}`, {withCredentials: false});
         this.files = response.data;
         console.log('----------this.files', this.files);
       } catch (error) {
--- src/components/TittleView.vue
+++ src/components/TittleView.vue
@@ -1,5 +1,5 @@
 <template>
     <div class="p-6 mb-10 bg-white rounded-xl shadow-md space-y-4 flex content-center">
-      <h2 class="text-4xl font-bold text-gray-900">INSTANCIA 1</h2>  
+      <h2 class="text-4xl font-bold text-gray-900">INSTANCIA 4</h2>  
     </div>
 </template>
