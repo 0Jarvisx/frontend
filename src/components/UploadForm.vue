f4040590e2fb800d4cd4de7fa39da770e36f5400
0Jarvisx <104813692+0Jarvisx@users.noreply.github.com>
2024-09-06 18:04:53 -0600

    Final Instancia 4

--- src/components/UploadForm.vue
+++ src/components/UploadForm.vue
@@ -49,7 +49,7 @@ export default {
       this.isUploading = true; 
 
       try {
-        const response = await axios.post('http://18.212.51.153:3000/upload?category=' + this.category, formData, {withCredentials: false});
+        const response = await axios.post('http://52.71.80.67:3000/upload?category=' + this.category, formData, {withCredentials: false});
         console.log('File uploaded:', response.data);
         if (response.data.message) this.$emit('file-uploaded', this.category);
         
